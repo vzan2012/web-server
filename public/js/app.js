f80cc28cd7d0d0a37e380a1b1b8d08f0f2e21b8d
Deepak Guptha <vzan2012@gmail.com>
2019-11-08 22:36:52 +0530

    feat(user): Implementation of HandleBars(hbs) for the pages

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log('Client Side - JavaScript File is loaded !!!');
---
+++ src/app.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+const express = require("express");
+const request = require("request");
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// Set the key value
+app.set("view engine", "hbs");
+
+// Home Page
+app.get("", (req, resp) => {
+  resp.render("index", {
+    title: "Weather - Home Page",
+    name: "vzan2012"
+  });
+});
+
+// About Page
+app.get("/about", (req, resp) => {
+  resp.render("about", {
+    title: "About Page",
+    name: "vzan2012"
+  });
+});
+
+// Help Page
+app.get("/help", (req, resp) => {
+  resp.render("help", {
+    title: "Help Page",
+    helpText: "This is some helpful text."
+  });
+  console.log("Viewing the Help Page");
+});
+
+// Enable the folder as the root in the server
+app.use(express.static(publicDirectoryPath));
+
+app.get("/weather", (req, resp) => {
+  resp.send({
+    forecast: "It is raining...",
+    location: "Paris"
+  });
+});
+
+// Display the Directory Path
+// console.log(__dirname);
+
+// Join the FilePath
+// console.log(path.join(__dirname, '../'));
+
+// Display the Current File Path
+// console.log(__filename);
+// console.log(path);
+
+app.listen(3000, () => console.log("Server is running in port 3000"));
