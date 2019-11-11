971b24690d1c31317cac87dfc36995e5703fbb97
Deepak Guptha <vzan2012@gmail.com>
2019-11-12 00:13:32 +0530

    feat(user): HTTP Requests with Fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-console.log('Client Side - JavaScript File is loaded !!!');
+console.log("Client Side - JavaScript File is loaded !!!");
+
+// Example
+// fetch('http://puzzle.mead.io/puzzle').then(response => {
+//     response.json().then(data => console.log(data));
+// }).catch(e => {
+//     console.log(e);
+// });
+
+// Goal: Fetch Weather
+fetch("http://localhost:3000/weather?address=boston")
+  .then(async resp => {
+    const data = await resp.json();
+    // console.log(data);
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  })
+  .catch(err => console.log(err));
